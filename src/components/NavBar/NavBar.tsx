8197e070219506769fe776a94390e7c0a6d161f6
yxxnhx <tjdbsgk514@hanmail.net>
2023-02-27 01:54:08 +0900

    [feat] header 구현
    navbar을 이용하여 기초 구현 및 간단한 스타일링 해두었습니다

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,67 @@
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+
+import Link from "next/link";
+import styled from "@emotion/styled";
+import { IconButton } from "@mui/material";
+
+const NavBar = () => {
+  const Logo = styled.div`
+    text-align: center;
+    font-size: 58px;
+    font-weight: bold;
+    margin: 2.5rem 0;
+  `;
+
+  const NavContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #000;
+  `;
+
+  const Nav = styled.nav`
+    display: flex;
+    justify-content: center;
+    font-size: 18px;
+    font-weight: 500;
+    margin: 0 auto;
+    padding-left: 5rem;
+    flex-grow: 1;
+    & a {
+      margin: 0 1rem;
+    }
+  `;
+  const Utils = styled.div`
+    flex-shrink: 0;
+
+    & button {
+      margin: 0 10px;
+    }
+  `;
+
+  return (
+    <div>
+      <Logo>glims</Logo>
+      <NavContainer>
+        <Nav>
+          <Link href="about">About</Link>
+          <Link href="best">Best</Link>
+          <Link href="brand">Brand</Link>
+          <Link href="review">Review</Link>
+        </Nav>
+        <Utils>
+          <IconButton color="primary" aria-label="search">
+            <SearchIcon />
+          </IconButton>
+          <IconButton color="primary" aria-label="user">
+            <PersonOutlineIcon />
+          </IconButton>
+        </Utils>
+      </NavContainer>
+    </div>
+  );
+};
+
+export default NavBar;
