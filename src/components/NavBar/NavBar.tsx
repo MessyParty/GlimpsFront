15c9c5f1d8ccff2c68ecd63a4fcb909c32e8d6b4
yxxnhx <tjdbsgk514@hanmail.net>
2023-03-01 22:30:04 +0900

    [fix & refactor] NavBar 스타일링 코드 함수 분리 및 href 경로 수정
    가독성을 위해 NavBar 스타일링 코드 함수 외부로 분리하였습니다
    redirect 이슈 방지를 위해 절대경로로 경로를 수정하였습니다

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,50 +6,15 @@ import styled from "@emotion/styled";
 import { IconButton } from "@mui/material";
 
 const NavBar = () => {
-  const Logo = styled.div`
-    text-align: center;
-    font-size: 58px;
-    font-weight: bold;
-    margin: 2.5rem 0;
-  `;
-
-  const NavContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding-bottom: 1rem;
-    border-bottom: 1px solid #000;
-  `;
-
-  const Nav = styled.nav`
-    display: flex;
-    justify-content: center;
-    font-size: 18px;
-    font-weight: 500;
-    margin: 0 auto;
-    padding-left: 5rem;
-    flex-grow: 1;
-    & a {
-      margin: 0 1rem;
-    }
-  `;
-  const Utils = styled.div`
-    flex-shrink: 0;
-
-    & button {
-      margin: 0 10px;
-    }
-  `;
-
   return (
     <div>
       <Logo>glims</Logo>
       <NavContainer>
         <Nav>
-          <Link href="about">About</Link>
-          <Link href="best">Best</Link>
-          <Link href="brand">Brand</Link>
-          <Link href="review">Review</Link>
+          <Link href="/about">About</Link>
+          <Link href="/best">Best</Link>
+          <Link href="/brand">Brand</Link>
+          <Link href="/review">Review</Link>
         </Nav>
         <Utils>
           <IconButton color="primary" aria-label="search">
@@ -63,5 +28,40 @@ const NavBar = () => {
     </div>
   );
 };
+
+const Logo = styled.div`
+  text-align: center;
+  font-size: 58px;
+  font-weight: bold;
+  margin: 2.5rem 0;
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #000;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  font-size: 18px;
+  font-weight: 500;
+  margin: 0 auto;
+  padding-left: 5rem;
+  flex-grow: 1;
+  & a {
+    margin: 0 1rem;
+  }
+`;
+const Utils = styled.div`
+  flex-shrink: 0;
+
+  & button {
+    margin: 0 10px;
+  }
+`;
 
 export default NavBar;
