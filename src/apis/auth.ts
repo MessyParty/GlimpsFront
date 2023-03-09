4d28819798b025f7898591a2e8c79709daf4abc4
Lyle Son <calends99@gmail.com>
2023-03-09 15:52:48 +0900

    [REFACTOR] Kakao Login - 코드 리뷰 내용 반영

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,14 +1,14 @@
 import api from ".";
 import { LoginResponse, RefreshResponse } from "./Interface/auth.interface";
 
-export const login = async (code: string): LoginResponse => {
+export const login = async (code: string): Promise<LoginResponse> => {
   const result = await api.get("/login/oauth2/code/kakao", {
     params: { code },
   });
   return result.data;
 };
 
-export const refresh = async (): RefreshResponse => {
+export const refresh = async (): Promise<RefreshResponse> => {
   const result = await api.post("/api/v1/session/access-token/issue");
   return result.data;
 };
