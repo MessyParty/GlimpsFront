e4a64d406f1c76d7d43c18c3122d5a0f716126fb
yxxnhx <tjdbsgk514@hanmail.net>
2023-03-30 01:11:15 +0900

    [fix] useInfiniteScroll 구조 변경
    사용하는 곳에서 구조분해할당하여 쓸 수 있도록 구조를 변경하였습니다

--- src/hooks/queries/useInfiniteScroll.ts
+++ src/hooks/queries/useInfiniteScroll.ts
@@ -5,19 +5,16 @@ import { getPerfume } from "@/apis/perfume";
 const useInfiniteScroll = () => {
   const QUERY_KEY = { Perfume: "perfume" };
 
-  const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } =
-    useInfiniteQuery(
-      [QUERY_KEY.Perfume],
-      ({ pageParam = 1 }) => getPerfume(pageParam),
-      {
-        getNextPageParam: (lastPage, allPages) => {
-          const nextPage = allPages.length + 1;
-          return lastPage?.length !== 0 ? nextPage : undefined;
-        },
+  return useInfiniteQuery(
+    [QUERY_KEY.Perfume],
+    ({ pageParam = 1 }) => getPerfume(pageParam),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length + 1;
+        return lastPage?.length !== 0 ? nextPage : undefined;
       },
-    );
-
-  return { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage };
+    },
+  );
 };
 
 export default useInfiniteScroll;
