a9ccd13e91a7c64ac774157251eb79636d2a338d
yxxnhx <tjdbsgk514@hanmail.net>
2023-04-08 00:12:38 +0900

    [feat] perfume mock data 생성

---
+++ __mocks__/PerfumeData.ts
@@ -0,0 +1,58 @@
+export const PerfumeData = [
+  {
+    id: 1,
+    brandName: "TAMBURINS",
+    perfumeName: "탬버린스",
+    score: 4.5,
+    imgSrc:
+      "https://www.trndf.com/news/data/20190709/p1065591406141189_714_thum.jpg",
+  },
+  {
+    id: 2,
+    brandName: "TAMBURINS",
+    perfumeName: "탬버린스",
+    score: 4.5,
+    imgSrc:
+      "https://www.trndf.com/news/data/20190709/p1065591406141189_714_thum.jpg",
+  },
+  {
+    id: 3,
+    brandName: "TAMBURINS",
+    perfumeName: "탬버린스",
+    score: 4.5,
+    imgSrc:
+      "https://www.trndf.com/news/data/20190709/p1065591406141189_714_thum.jpg",
+  },
+  {
+    id: 4,
+    brandName: "TAMBURINS",
+    perfumeName: "탬버린스",
+    score: 4.5,
+    imgSrc:
+      "https://www.trndf.com/news/data/20190709/p1065591406141189_714_thum.jpg",
+  },
+  {
+    id: 5,
+    brandName: "TAMBURINS",
+    perfumeName: "탬버린스",
+    score: 4.5,
+    imgSrc:
+      "https://www.trndf.com/news/data/20190709/p1065591406141189_714_thum.jpg",
+  },
+  {
+    id: 6,
+    brandName: "TAMBURINS",
+    perfumeName: "탬버린스",
+    score: 4.5,
+    imgSrc:
+      "https://www.trndf.com/news/data/20190709/p1065591406141189_714_thum.jpg",
+  },
+  {
+    id: 7,
+    brandName: "TAMBURINS",
+    perfumeName: "탬버린스",
+    score: 4.5,
+    imgSrc:
+      "https://www.trndf.com/news/data/20190709/p1065591406141189_714_thum.jpg",
+  },
+];
