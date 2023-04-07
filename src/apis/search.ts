a723dba33eb90701d898dad706c964ab10746d96
yxxnhx <tjdbsgk514@hanmail.net>
2023-04-07 15:36:24 +0900

    [fix] search api 수정

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import type { Search } from "./Interface/search.interface";
 
 export const getSearch = async (
-  brandname: string,
-  perfumename: string,
-  notes: string,
+  brandname?: string,
+  perfumename?: string,
+  notes?: string,
 ): Promise<Search[]> => {
-  const { data } = await axios.get<Search[]>(`/search`, {
+  const { data } = await axios.get<Search[]>(`/perfumes/search`, {
     params: {
       brandname,
       perfumename,
