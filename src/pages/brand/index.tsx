252e51a157d29335696f99103f22257717473f32
yxxnhx <tjdbsgk514@hanmail.net>
2023-04-08 00:16:16 +0900

    [add] brand page 서버 설정

--- src/pages/brand/index.tsx
+++ src/pages/brand/index.tsx
@@ -1,5 +1,25 @@
 import Brand from "@/components/Brand";
+import { GetServerSideProps } from "next";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { getBrand } from "@/apis/brand";
 
 export default function BrandPage() {
   return <Brand />;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const queryClient = new QueryClient();
+
+  await Promise.allSettled([
+    queryClient.prefetchQuery({
+      queryKey: ["brand", "best"],
+      queryFn: () => getBrand(),
+    }),
+  ]);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
