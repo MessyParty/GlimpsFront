1bd798babc28803ea56ef32aba9eaaea257b027a
yxxnhx <tjdbsgk514@hanmail.net>
2023-04-11 23:58:12 +0900

    [feat] searchPerfumes api 추가

--- src/apis/perfume.ts
+++ src/apis/perfume.ts
@@ -8,9 +8,21 @@ export const getPerfume = async (rid: number): Promise<Perfume[]> => {
 };
 
 export const getBestPerfume = async (num: number): Promise<Perfume[]> => {
-  const { data } = await axios.get<Perfume[]>("/perfumes/bestPerfumes", {
+  const { data } = await axios.get<Perfume[]>("/perfumes/best", {
     params: {
       amountPerfumes: num,
+    },
+  });
+  return data;
+};
+
+export const searchPerfumes = async (
+  brand: string,
+  pageParam?: number,
+): Promise<Perfume[]> => {
+  const { data } = await axios.get<Perfume[]>("/perfumes", {
+    params: {
+      brand: brand,
     },
   });
   return data;
