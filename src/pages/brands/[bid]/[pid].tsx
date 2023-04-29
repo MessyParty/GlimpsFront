aaaa39b0a42faf83b2716b8d7fc7ebee268c6a53
syoo970 <syu9710@gmail.com>
2023-04-29 15:55:41 +0900

    [STYLE] score 정보 api 연동 및 styling

--- src/pages/brands/[bid]/[pid].tsx
+++ src/pages/brands/[bid]/[pid].tsx
@@ -9,6 +9,7 @@ import { Typography, Divider } from "@mui/material";
 import Spacer from "@/components/Spacer";
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+import Rating from "@/components/Rating";
 
 type DetailType = {
   pid: string;
@@ -22,7 +23,7 @@ const DetailPage = () => {
 
   return (
     <PageContainer>
-      <HeaderSection>
+      <StyledSection withImg>
         <div className="item">
           <img src={data?.photos[0].url} alt="perfume image" />
         </div>
@@ -32,19 +33,71 @@ const DetailPage = () => {
               <Typography variant="h2">{data?.brandName}</Typography>
               <Typography variant="h5">{data?.perfumeName}</Typography>
             </div>
-            <div className="notes">
-              {data?.notes.map(({ id, engName, korName }) => (
-                <Notes key={id}>
-                  <Typography>{engName}</Typography>
-                  <Typography>{korName}</Typography>
-                </Notes>
-              ))}
-            </div>
           </HeaderInfo>
         </div>
-      </HeaderSection>
+      </StyledSection>
       <StyledDivider position="right" />
-      <Spacer />
+      <Spacer y={2} />
+      <StyledSection>
+        <div className="item">
+          <NotesInfo>
+            <Typography variant="h5">NOTES</Typography>
+            <Spacer />
+            {data?.notes.map(({ id, engName, korName }) => (
+              <Notes key={id}>
+                <Typography>{engName}</Typography>
+                <Typography>{korName}</Typography>
+              </Notes>
+            ))}
+          </NotesInfo>
+        </div>
+        <BlackDivider flexItem orientation="vertical" variant="middle" />
+        <div className="item">
+          <ScoreInfo>
+            <Typography variant="h5">SCORE</Typography>
+            <Spacer />
+            <Scores>
+              <Score>
+                <div className="info">
+                  <BoldTypo variant="subtitle1">지속력</BoldTypo>
+                  <Typography variant="subtitle2">LONGEVITY</Typography>
+                </div>
+                <Rating
+                  precision={0.5}
+                  value={data?.longevityRatings}
+                  sx={{ fontSize: "45px" }}
+                />
+              </Score>
+              <Score>
+                <div className="info">
+                  <BoldTypo variant="subtitle1">잔향</BoldTypo>
+                  <Typography variant="subtitle2">SILLAGE</Typography>
+                </div>
+                <Rating
+                  precision={0.5}
+                  value={data?.sillageRatings}
+                  sx={{ fontSize: "45px" }}
+                />
+              </Score>
+              <Spacer y={2} />
+              <Score>
+                <div className="info">
+                  <BoldTypo variant="h6">AVERAGE</BoldTypo>
+                  <BoldTypo variant="h6">SCORE</BoldTypo>
+                </div>
+                <Rating
+                  precision={0.5}
+                  value={data?.overallRatings}
+                  sx={{ fontSize: "50px" }}
+                />
+              </Score>
+            </Scores>
+          </ScoreInfo>
+        </div>
+      </StyledSection>
+      <Spacer y={2} />
+      <StyledDivider position="left" />
+      <Spacer y={2} />
     </PageContainer>
   );
 };
@@ -89,26 +142,44 @@ const PageContainer = styled.div`
   flex-direction: column;
 `;
 
-const HeaderSection = styled.section`
+type SectionProps = { withImg?: boolean };
+
+const StyledSection = styled.section<SectionProps>`
   display: flex;
   & > .item {
-    flex-basis: 50%;
+    flex: 1;
     max-height: 500px;
-    & > img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
+    ${(props) =>
+      props.withImg
+        ? css`
+            & > img {
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+            }
+          `
+        : null}
   }
 `;
 
-const HeaderInfo = styled.div`
+const Info = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
+`;
+
+const HeaderInfo = styled(Info)`
   justify-content: space-evenly;
   padding: 1rem;
+`;
+
+const NotesInfo = styled(Info)`
+  padding: 2rem;
+`;
+
+const ScoreInfo = styled(Info)`
+  padding: 2rem;
 `;
 
 const Notes = styled.div`
@@ -116,8 +187,28 @@ const Notes = styled.div`
   gap: 10px;
 `;
 
+const Scores = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Score = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  & > .info {
+    display: flex;
+    flex-direction: column;
+    flex-basis: 30%;
+  }
+`;
+
 const BlackDivider = styled(Divider)`
   border-color: black;
+`;
+
+const BoldTypo = styled(Typography)`
+  font-weight: bolder;
 `;
 
 const Square = styled.div<{ pos: "left" | "right"; w: string }>`
