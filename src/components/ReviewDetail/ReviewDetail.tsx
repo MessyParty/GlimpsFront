77f8e7296e51f1d4a10a5b596e2be1bac942d5a7
yxxnhx <tjdbsgk514@hanmail.net>
2023-05-01 22:49:58 +0900

    [feat] review 수정기능 추가

--- src/components/ReviewDetail/ReviewDetail.tsx
+++ src/components/ReviewDetail/ReviewDetail.tsx
@@ -1,247 +1,243 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import styled from "@emotion/styled";
 
 import { Typography } from "@mui/material";
 import { QuoteLeft, QuoteRight } from "../CustomIcon";
-import { CardMedia, CardProps } from "@mui/material";
+import { CardMedia } from "@mui/material";
 
 import Rating from "../Rating";
-import Tag from "../Tag";
 import Button from "../Button";
-import { deleteReview, updateReview } from "@/apis/review";
-import { useRecoilState } from "recoil";
+
 import Modal from "../Modal";
 import ReviewModal from "../ReviewModal";
+import { deleteReview } from "@/apis/review";
 import { MODAL_KEYS } from "@/constants/modalKeys";
 import useModal from "@/hooks/useModal";
-
-interface ReviewDetailProps extends CardProps {
-  title: string;
-  author: string;
-  score: number;
+import useReview from "@/hooks/queries/useReview";
+interface ReviewData {
+  perfumeBrandEng: string;
+  body: string;
+  createdAt: string;
+  heartCnt: number;
   longevityRatings: number;
-  sillageRatings: number;
-  tags: string[];
-  description?: string;
-  photoUrl?: string[];
-  perfumeName: string;
+  nickname: string;
+  overallRatings: number;
   perfumeBrand: string;
-  createAt: string;
-  id: string;
+  perfumeName: string;
+  photoUrls: string[];
+  sillageRatings: number;
+  title: string;
+  tags?: string[];
+  uuid: string;
+  scentRatings: number;
 }
 
-export default function ReviewDetail({
-  title,
-  author,
-  score,
-  sillageRatings,
-  longevityRatings,
-  tags,
-  description,
-  photoUrl,
-  perfumeName,
-  perfumeBrand,
-  createAt,
-  id,
-  ...props
-}: ReviewDetailProps) {
+export default function ReviewDetail({ ...props }) {
+  const [reviewData, setReviewData] = useState({});
+
   const router = useRouter();
-  const { isOpen } = useModal(MODAL_KEYS.review);
+  const reviewModal = useModal(MODAL_KEYS.review);
+
+  const { pid } = router.query;
+  const { data } = useReview(pid as string);
 
   const handleDelete = (id: string) => {
     deleteReview(id);
   };
 
-  const handleUpdateReview = async (rid, data) => {
-    try {
-      const updatedReview = await updateReview(rid, data);
-      console.log(updatedReview);
-    } catch (error) {
-      console.log(error);
-    }
+  const handleUpdateReview = (data: ReviewData) => {
+    reviewModal.openModal();
+    setReviewData(data);
   };
 
   const moveToBack = () => {
     router.back();
   };
 
-  const date = new Date(createAt);
+  const date = new Date(data?.createdAt as string);
   const formattedDate = `${date.getFullYear()}.${(date.getMonth() + 1)
     .toString()
     .padStart(2, "0")}.${date.getDate().toString().padStart(2, "0")}`;
 
   return (
     <>
-      <Container {...props}>
-        <ReviewTopBox>
-          <Typography fontSize={30}>{perfumeName}</Typography>
-          <Typography fontSize={23}>{perfumeBrand}</Typography>
-        </ReviewTopBox>
+      {data ? (
+        <>
+          <Container {...props}>
+            <ReviewTopBox>
+              <Typography fontSize={30}>{data.perfumeName}</Typography>
+              <Typography fontSize={23}>{data.perfumeBrandEng}</Typography>
+            </ReviewTopBox>
 
-        <ReviewTitleBox>
-          <Typography fontSize={20}>{title}</Typography>
-          <div className="review-info">
-            <Typography fontSize={18}>{formattedDate}</Typography>
-            <Typography fontSize={18}>by {author}</Typography>
-          </div>
-        </ReviewTitleBox>
-        <Quote>
-          <QuoteLeft />
-          <Typography
-            variant="h5"
-            component="blockquote"
-            fontSize="28px"
-            margin="0 12px"
-            paddingTop="20px"
-          >
-            {title}
-          </Typography>
-          <QuoteRight />
-        </Quote>
-        <div>
-          {photoUrl?.map((photo, index) => (
-            <CardMedia
-              component="img"
-              src={photo}
-              alt="review image"
-              width={400}
-              height={400}
-              style={{ minWidth: "400px", minHeight: "400px" }}
-              key={index}
-            />
-          ))}
-        </div>
-        <ReviewContent>
-          {tags?.map((tag, index) => (
-            <Tag key={index} content={tag} />
-          ))}
-        </ReviewContent>
-        <ReviewContent>
-          <div className="rating-title">
-            <Typography fontWeight="bold" fontSize={21}>
-              AVERAGE
-            </Typography>
-            <Typography fontWeight="bold" fontSize={21}>
+            <ReviewTitleBox>
+              <Typography fontSize={20}>{data.title}</Typography>
+              <div className="review-info">
+                <Typography fontSize={18}>{formattedDate}</Typography>
+                <Typography fontSize={18}>by {data.nickname}</Typography>
+              </div>
+            </ReviewTitleBox>
+            <Quote>
+              <QuoteLeft />
+              <Typography
+                variant="h5"
+                component="blockquote"
+                fontSize="28px"
+                margin="0 12px"
+                paddingTop="20px"
+              >
+                {data.title}
+              </Typography>
+              <QuoteRight />
+            </Quote>
+            <div>
+              {data.photoUrls?.map((photo, index) => (
+                <CardMedia
+                  component="img"
+                  src={photo}
+                  alt="review image"
+                  width={400}
+                  height={400}
+                  style={{ minWidth: "400px", minHeight: "400px" }}
+                  key={index}
+                />
+              ))}
+            </div>
+            <ReviewContent>
+              <div className="rating-title">
+                <Typography fontWeight="bold" fontSize={21}>
+                  AVERAGE
+                </Typography>
+                <Typography fontWeight="bold" fontSize={21}>
+                  SCORE
+                </Typography>
+              </div>
+              <Rating
+                sx={{ fontSize: "48px" }}
+                value={data.overallRatings ?? 1.5}
+                precision={0.5}
+                size="large"
+              />
+            </ReviewContent>
+
+            <Typography fontWeight="bold" fontSize={25}>
               SCORE
             </Typography>
-          </div>
-          <Rating
-            sx={{ fontSize: "48px" }}
-            value={1.5}
-            precision={0.5}
-            size="large"
-          />
-        </ReviewContent>
+            <ReviewRatingContent>
+              <div className="rating-title">
+                <Typography fontWeight="bold" fontSize={20}>
+                  본연의 향
+                </Typography>
+                <Typography fontSize={18} color="#474747">
+                  SCENT
+                </Typography>
+              </div>
+              <Rating
+                sx={{ fontSize: "48px" }}
+                value={data.scentRatings ?? 1.5}
+                precision={0.5}
+                size="large"
+              />
+            </ReviewRatingContent>
+            <ReviewRatingContent>
+              <div className="rating-title">
+                <Typography fontWeight="bold" fontSize={20}>
+                  지속력
+                </Typography>
+                <Typography fontSize={18} color="#474747">
+                  LONGEVITY
+                </Typography>
+              </div>
+              <Rating
+                sx={{ fontSize: "48px" }}
+                value={data.longevityRatings ?? 1.5}
+                precision={0.5}
+                size="large"
+              />
+            </ReviewRatingContent>
+            <ReviewRatingContent>
+              <div className="rating-title">
+                <Typography fontWeight="bold" fontSize={20}>
+                  잔향
+                </Typography>
+                <Typography fontSize={18} color="#474747">
+                  SILLAGE
+                </Typography>
+              </div>
+              <Rating
+                sx={{ fontSize: "48px" }}
+                value={data.sillageRatings ?? 1.5}
+                precision={0.5}
+                size="large"
+              />
+            </ReviewRatingContent>
 
-        <Typography fontWeight="bold" fontSize={25}>
-          SCORE
-        </Typography>
-        <ReviewRatingContent>
-          <div className="rating-title">
-            <Typography fontWeight="bold" fontSize={20}>
-              본연의 향
-            </Typography>
-            <Typography fontSize={18} color="#474747">
-              SCENT
-            </Typography>
-          </div>
-          <Rating
-            sx={{ fontSize: "48px" }}
-            value={score ?? 1.5}
-            precision={0.5}
-            size="large"
-          />
-        </ReviewRatingContent>
-        <ReviewRatingContent>
-          <div className="rating-title">
-            <Typography fontWeight="bold" fontSize={20}>
-              지속력
-            </Typography>
-            <Typography fontSize={18} color="#474747">
-              LONGEVITY
-            </Typography>
-          </div>
-          <Rating
-            sx={{ fontSize: "48px" }}
-            value={longevityRatings ?? 1.5}
-            precision={0.5}
-            size="large"
-          />
-        </ReviewRatingContent>
-        <ReviewRatingContent>
-          <div className="rating-title">
-            <Typography fontWeight="bold" fontSize={20}>
-              잔향
-            </Typography>
-            <Typography fontSize={18} color="#474747">
-              SILLAGE
-            </Typography>
-          </div>
-          <Rating
-            sx={{ fontSize: "48px" }}
-            value={sillageRatings ?? 1.5}
-            precision={0.5}
-            size="large"
-          />
-        </ReviewRatingContent>
-
-        <ReviewDescription>
-          <Typography fontSize={20} fontWeight="light">
-            {description}
-          </Typography>
-        </ReviewDescription>
-        <div className="button-box">
-          <Button
-            variant="outlined"
-            style={{
-              borderRadius: 0,
-              fontSize: "17px",
-              padding: "10px 40px",
-              margin: "0 1rem",
-            }}
-            onClick={() => handleUpdateReview(id, title)}
-          >
-            수정하기
-          </Button>
-          <Modal
-            modalKey={MODAL_KEYS.review}
-            open={isOpen}
-            content={<ReviewModal perfumeUuid={id} perfumeName={perfumeName} />}
-            fullWidth
-            maxWidth="lg"
-          />
-          <Button
-            variant="outlined"
-            style={{
-              borderRadius: 0,
-              fontSize: "17px",
-              padding: "10px 40px",
-              margin: "0 1rem",
-            }}
-            onClick={() => handleDelete}
-          >
-            삭제하기
-          </Button>
-        </div>
-      </Container>
-      <ListButton>
-        <Button
-          className="list-button"
-          variant="contained"
-          style={{
-            backgroundColor: "#000",
-            color: "#fff",
-            borderRadius: 0,
-            fontSize: "17px",
-            padding: "10px 40px",
-            margin: "0 1rem",
-          }}
-          onClick={moveToBack}
-        >
-          목록
-        </Button>
-      </ListButton>
+            <ReviewDescription>
+              <Typography fontSize={20} fontWeight="light">
+                {data.body}
+              </Typography>
+            </ReviewDescription>
+            <div className="button-box">
+              <Button
+                variant="outlined"
+                style={{
+                  borderRadius: 0,
+                  fontSize: "17px",
+                  padding: "10px 40px",
+                  margin: "0 1rem",
+                }}
+                onClick={() => handleUpdateReview(data)}
+              >
+                수정하기
+              </Button>
+              <Modal
+                modalKey={MODAL_KEYS.review}
+                open={reviewModal.isOpen}
+                content={
+                  <ReviewModal
+                    perfumeUuid={data.uuid}
+                    perfumeName={data.perfumeName}
+                    reviewData={reviewData}
+                  />
+                }
+                fullWidth
+                maxWidth="lg"
+              />
+              <Button
+                variant="outlined"
+                style={{
+                  borderRadius: 0,
+                  fontSize: "17px",
+                  padding: "10px 40px",
+                  margin: "0 1rem",
+                }}
+                onClick={() => handleDelete}
+              >
+                삭제하기
+              </Button>
+            </div>
+          </Container>
+          <ListButton>
+            <Button
+              className="list-button"
+              variant="contained"
+              style={{
+                backgroundColor: "#000",
+                color: "#fff",
+                borderRadius: 0,
+                fontSize: "17px",
+                padding: "10px 40px",
+                margin: "0 1rem",
+              }}
+              onClick={moveToBack}
+            >
+              목록
+            </Button>
+          </ListButton>
+        </>
+      ) : (
+        <p>데이터가 없습니다</p>
+      )}
     </>
   );
 }
